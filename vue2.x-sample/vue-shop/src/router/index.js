0baf1d174b8267a2e31fb0a8a458be35956589ab
tqyao <790461387@qq.com>
2021-05-14 17:47:32 +0800

    first commit

---
+++ vue2.x-sample/vue-shop/src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../components/Login'
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/login',
+        component: Login
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
